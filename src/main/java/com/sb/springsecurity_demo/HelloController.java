71d6b9f865cf95680b72fd329e7850b9156f1f04
Bharti <sahanibharti71@gmail.com>
2026-02-24 18:50:24 +0530

    username password set in properties file, tested on browser network tab for session cookie, tested on postman with basic auth

--- src/main/java/com/sb/springsecurity_demo/HelloController.java
+++ src/main/java/com/sb/springsecurity_demo/HelloController.java
@@ -1,19 +1,22 @@
 package com.sb.springsecurity_demo;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpRequest;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String Hello() {
-        return "Hello, World!";
+    public String Hello(HttpServletRequest req) {
+        return "Hello, World!" + req.getSession().getId();
     }
 
     @GetMapping("/about")
-    public String About() {
-        return "Spring Security Demo Application";
+    public String About(HttpServletRequest req) {
+        return "Spring Security Demo Application"  + req.getSession().getId();
     }
 
 

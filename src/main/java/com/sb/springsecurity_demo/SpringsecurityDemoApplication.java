22758f869608247edf47d3b0b724728c41373c64
Bharti <sahanibharti71@gmail.com>
2026-03-14 21:09:20 +0530

    implement spring security config: disable csrf, stateless sessions, basic auth, and test formLogin vs httpBasic behavior with practical observations

--- src/main/java/com/sb/springsecurity_demo/SpringsecurityDemoApplication.java
+++ src/main/java/com/sb/springsecurity_demo/SpringsecurityDemoApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringsecurityDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecurityDemoApplication.class, args);
+
+        SpringApplication.run(SpringsecurityDemoApplication.class, args);
 	}
 
 }
